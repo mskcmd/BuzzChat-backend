6337a2d916b3894c98d398c3e2722ca22accb548
Muhammed Suhail <wwwsuhail883@gmail.com>
2024-09-11 20:32:42 +0530

    change mongodb code

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -5,15 +5,11 @@ const MONGO_URI = "mongodb://localhost:27017/BuzzChat";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
-    process.exit(1);
-  }
+    process.exit(1);  }
 };
 
 module.exports = { connectDB };
