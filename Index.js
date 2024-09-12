4ab89467b0049f52f60511b424c8c12595efae08
Muhammed Suhail <wwwsuhail883@gmail.com>
2024-09-12 11:08:15 +0530

    add atles link

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { chats } = require("./Dummy/data");
+require('dotenv').config()
 const { connectDB } = require("./config/mongoDb");
 const { notFound, errorHandler } = require("./middleware/errorHandlers");
 const userRoute = require("./routes/userRoute");
